ee71c06b8dd573f025a203e712d85822aab065e6
Ryan Hellyer <ryanhellyer@gmail.com>
2015-11-15 22:22:42 +0100

    Google Analytics tracking code

--- views/header.php
+++ views/header.php
@@ -5,8 +5,20 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<title><?php echo esc_html( $this->_get_page_title() ); ?></title>
+	<link rel="stylesheet" href="<?php echo COMICJET_ASSETS_URL; ?>/style.css" type="text/css" media="all" /><?php
+	if ( defined( 'COMICJET_ANALYTICS' ) ) {
+		echo "
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', '" . COMICJET_ANALYTICS . "', 'auto');
+	ga('send', 'pageview');
+	</script>";
+	}
+	?>
 
-	<link rel="stylesheet" href="<?php echo COMICJET_ASSETS_URL; ?>/style.css" type="text/css" media="all" />
 </head>
 <body class="<?php echo esc_attr( $template ); ?>">
 
